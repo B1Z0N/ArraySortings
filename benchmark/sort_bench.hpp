7324265e965623a791f0ed3f81cf24e5c130e86f
B1Z0N <32279961+B1Z0N@users.noreply.github.com>
2019-05-01 21:53:33 +0300

    Added benchmarking classes, mostly implemented it, not tested. TODO: make preconditions available

--- benchmark/sort_bench.hpp
+++ benchmark/sort_bench.hpp
@@ -1,16 +1,166 @@
+#ifndef AOS_LAB2_SORT_BENCH
+#define AOS_LAB2_SORT_BENCH
+
+#include <tuple>  // std::tuple
+#include <vector> // std::vector
+
+#include "sort_bench_interface.hpp"
+
+namespace SortingBenchmark
+{
+//////////-GenFunc-//////////////
+
+template <typename AElement>
+GenFunc<AElement>::GenFunc(GenFunc<AElement>::genf genf, AElement _min, AElement _max)
+    : type{0}, gf{genf}, min{_min}, max{_max}
+{
+}
+
+template <typename AElement>
+GenFunc<AElement>::GenFunc(GenFunc<AElement>::const_genf genf, const AElement &_min, const AElement &_max)
+    : type{1}, cgf{genf}, min{_min}, max{_max}
+{
+}
+
+template <typename AElement>
+AElement GenFunc<AElement>::operator()()
+{
+    switch (type)
     {
-        switch(type)
-        {
-            case 0:
-                return gf(min, max);
-                break;
-            case 1:
-                return cgf(min, max);
-                break;
-        }
+    case 0:
+        return gf(min, max);
+        break;
+    case 1:
+        return cgf(min, max);
+        break;
     }
+}
 
+//////////-SortFunc-//////////////
 
-        {
-        sorting(func_args...);
-    }
+template <template <typename> typename A, typename AE /*, typename ...PreconditionReturnTypes */>
+SortFunc<A, AE>::SortFunc(SortFunc<A, AE>::sortf _sorting) : sorting{_sorting}
+{
+}
+
+template <template <typename> typename A, typename AE /*, typename ...PreconditionReturnTypes */>
+void SortFunc<A, AE>::sort()
+{
+    if (arr == nullptr)
+        throw ArrayNotGiven{};
+
+    sorting(*arr);
+}
+
+//////////-ArrayElement-//////////////
+template <typename T>
+uint ArrayElement<T>::comparisons = 0;
+template <typename T>
+uint ArrayElement<T>::assignments = 0;
+
+template <typename T>
+bool ArrayElement<T>::cmp_on = true;
+template <typename T>
+bool ArrayElement<T>::asgn_on = true;
+
+template <typename T>
+ArrayElement<T>::ArrayElement(const T &el)
+    : elem{el}
+{
+}
+
+template <typename T>
+ArrayElement<T>::ArrayElement(T &&el)
+    : elem{std::move(el.elem)}
+{
+}
+
+template <typename T>
+ArrayElement<T> &ArrayElement<T>::operator=(const ArrayElement<T> &el)
+{
+    if (asgn_on)
+        assignments++;
+    elem = el.elem;
+
+    return (*this);
+}
+template <typename T>
+ArrayElement<T> &ArrayElement<T>::operator=(const T &el)
+{
+    if (asgn_on)
+        assignments++;
+    elem = el;
+
+    return (*this);
+}
+template <typename T>
+ArrayElement<T> &ArrayElement<T>::operator=(ArrayElement<T> &&el)
+{
+    if (asgn_on)
+        assignments++;
+    elem = std::move(el.elem);
+
+    return (*this);
+}
+template <typename T>
+ArrayElement<T> &ArrayElement<T>::operator=(T &&el)
+{
+    if (asgn_on)
+        assignments++;
+    elem = std::move(el);
+
+    return (*this);
+}
+
+template <typename T>
+inline void ArrayElement<T>::reset()
+{
+    comparisons = 0;
+    assignments = 0;
+}
+
+template <typename T>
+inline bool operator==(const ArrayElement<T> &a, const ArrayElement<T> &b)
+{
+    if (a.cmp_on)
+        a.comparisons++;
+    return a.elem == b.elem;
+}
+template <typename T>
+inline bool operator!=(const ArrayElement<T> &a, const ArrayElement<T> &b)
+{
+    if (a.cmp_on)
+        a.comparisons++;
+    return a.elem != b.elem;
+}
+template <typename T>
+inline bool operator>(const ArrayElement<T> &a, const ArrayElement<T> &b)
+{
+    if (a.cmp_on)
+        a.comparisons++;
+    return a.elem > b.elem;
+}
+template <typename T>
+inline bool operator<(const ArrayElement<T> &a, const ArrayElement<T> &b)
+{
+    if (a.cmp_on)
+        a.comparisons++;
+    return a.elem < b.elem;
+}
+template <typename T>
+inline bool operator>=(const ArrayElement<T> &a, const ArrayElement<T> &b)
+{
+    if (a.cmp_on)
+        a.comparisons++;
+    return a.elem >= b.elem;
+}
+template <typename T>
+inline bool operator<=(const ArrayElement<T> &a, const ArrayElement<T> &b)
+{
+    if (a.cmp_on)
+        a.comparisons++;
+    return a.elem <= b.elem;
+}
+} // namespace SortingBenchmark
+
+#endif
--- benchmark/sort_bench_interface.hpp
+++ benchmark/sort_bench_interface.hpp
@@ -1,17 +1,14 @@
+#ifndef AOS_LAB2_SORT_BENCH_INTERFACE
+
+#define AOS_LAB2_SORT_BENCH_INTERFACE
 #include <tuple>  // std::tuple
 #include <vector> // std::vector
 
 namespace SortingBenchmark
 {
 
-template <typename Array, typename ArrayElement>
+template <template <typename> typename Array, typename ArrayElement>
 using sortf = void (*)(Array<ArrayElement> &); // pointer to sorting function
-
-template <typename ArrayElement> // pointer to function, that generates elements in range (min, max)
-using genfunc = ArrayElement (*)(ArrayElement min, ArrayElement max);
-
-template <typename ArrayElement>                     // pointer to function, that does the same
-using const_genfunc = genfunc<const ArrayElement &>; // but passing const refs, because of cheapness of this operation
 
 using uint = unsigned int;
 
@@ -21,10 +18,17 @@ class GenFunc
 // with const refs arguments and with ordinary arguments
 // allows calling function not even thinking about what it's type
 {
+    // pointer to function, that generates elements in range (min, max)
+    using genf = ArrayElement (*)(ArrayElement min, ArrayElement max);
+
+    // pointer to function, that does the same
+    // but passing const refs, because of cheapness of this operation
+    using const_genf = ArrayElement (*)(const ArrayElement &, const ArrayElement &);
+
     bool type; // 0 - first(gf), 1 - second(cgf)
     union {
-        genfunc gf;
-        const_genfunc cgf;
+        genf gf;
+        const_genf cgf;
     };
 
 public:
@@ -32,44 +36,120 @@ public:
     ArrayElement max;
 
     GenFunc() = delete;
-    GenFunc(genfunc<ArrayElement>);       // constructor for 1st type
-    GenFunc(const_genfunc<ArrayElement>); // constructor for 2nd type
+    GenFunc(genf genf, ArrayElement _min, ArrayElement _max);                     // constructor for 1st type
+    GenFunc(const_genf genf, const ArrayElement &_min, const ArrayElement &_max); // constructor for 2nd type
 
     ArrayElement operator()(); // for calling just like ordinary generating function
-}
+};
 
-template <typename Array, typename ArrayElement>
+// template <typename Array, typename ArrayElement, typename ReturnType>
+// using precondf = ReturnType (*)(const Array<ArratElement>&);
+
+// template <typename Array, typename ArrayElement>
+// using precondf = ReturnType (*)(const Array<ArratElement>&);
+
+template <template <typename> typename Array, typename ArrayElement /*, typename ...PreconditionReturnTypes */>
 class SortFunc
 {
-    typedef sortf<Array, ArrayElement> cur_sortf;
-    // precondition functions that return values and take only array
+    // template <typename ReturnType>
+    // using precondf = ReturnType (*)(const Array<ArrayElement>&);
 
-    // preconditions as the results of precondition functions
-    // sorting function that return nothing and take only array
+    using sortf = void (*)(Array<ArrayElement> &);
+
+    // precondition functions that return values and take only array
+    // std::tuple<precondf<PreconditionReturnTypes>...> preconditions;
+    // std::tuple<PreconditionReturnTypes...> returns;
+    // sorting function that returns nothing and take only array
+    sortf sorting;
 
 public:
+    Array<ArrayElement> *arr;
+
+    class ArrayNotGiven
+    {
+    };
+
     SortFunc() = delete;
-    SortFunc()
-}
+    SortFunc(sortf);
+
+    // void precond();
+    void sort();
+};
 
 template <typename T>
 class ArrayElement
 // class that allows comparison and assignment counting
 {
     T elem;
-
+    static uint comparisons; // 0 by default
+    static uint assignments; // 0 by default
 public:
-    // implements operators
+    static bool cmp_on;  // true by default
+    static bool asgn_on; // true by default
+
+    ArrayElement() = delete;
+    ArrayElement(const T &);
+    ArrayElement(T &&);
+
+    ArrayElement &operator=(const ArrayElement &);
+    ArrayElement &operator=(const T &);
+    ArrayElement &operator=(ArrayElement &&);
+    ArrayElement &operator=(T &&);
+
+    void reset();
+
+    template <typename U>
+    friend bool operator==(const ArrayElement<U> &, const ArrayElement<U> &);
+    template <typename U>
+    friend bool operator!=(const ArrayElement<U> &, const ArrayElement<U> &);
+    template <typename U>
+    friend bool operator<(const ArrayElement<U> &, const ArrayElement<U> &);
+    template <typename U>
+    friend bool operator>(const ArrayElement<U> &, const ArrayElement<U> &);
+    template <typename U>
+    friend bool operator<=(const ArrayElement<U> &, const ArrayElement<U> &);
+    template <typename U>
+    friend bool operator>=(const ArrayElement<U> &, const ArrayElement<U> &);
+};
+
+template <typename ArrayElement>
+struct SortStats
+{
+    time_t time;
+    uint len;
+    
+    ArrayElement min;
+    ArrayElement max;
+
+    uint comparisons;
+    uint assignments;
 }
 
-template <typename Array, typename ArrayElement>
+template <template <typename> typename Array, typename ArrayElement>
 class SortBench
 // implements time testing
 // implements comparison and assignment counting
 {
-    uint amount;
-    GenFunc<ArrayElement> generating;
+    using SortFunc<Array, ArrayElement>::sortf;
+    using GenFunc<ArrayElement>::genf;
+    using GenFunc<ArrayElement>::const_genf;
 
+    SortStats<ArrayElement> stats;
+    GenFunc<ArrayElement> generating;
+    SortFunc<Array, ArrayElement> sorting;
+    Array<ArrayElement> array;
+
+protected:
+    void find_comparisons_and_assignments();
+    void time_it();
 public:
+    SortBench() = delete;
+    SortBench(sortf sort, genf generate, ArrayElement min, ArrayElement max);
+    SortBench(sortf sort, const_genf generate, const ArrayElement& min, const ArrayElement& max);
+
+    void gen_array();
+    SortStats<ArrayElement> get_stats() const;
 };
 } // namespace SortingBenchmark
+
+#endif
