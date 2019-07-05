fed8dade19306f7dfa447d7b5e8c8e797e9ca6ba
B1Z0N <kolausf@gmail.com>
2019-07-05 14:41:00 +0300

    Added genfunc class, not tested yet

--- benchmark/sort_bench_interface.hpp
+++ benchmark/sort_bench_interface.hpp
@@ -8,7 +8,6 @@
 
 namespace srtbch
 {
-
 struct CmpAsgn
 {
     size_t cmp;
@@ -16,15 +15,16 @@ struct CmpAsgn
 };
 
 using SortStats = std::vector<std::tuple<
-                                size_t,                     // array length
-                                std::chrono::duration,      // sorting duration
-                                CmpAsgn                     // comparisons and assignments
-                                >>
+                  size_t,                     // array length
+                  std::chrono::duration,      // sorting duration
+                  CmpAsgn                     // comparisons and assignments
+                  >>
 
-template <typename T, typename GenFunc, template <typename> typename SortFunctor>
+                  template <typename T, typename GenFunc, template <typename> typename SortFunctor>
 // set limits on types and maybe even concepts
 // look forward to thread safety
 // look forward to array + - and so on operations on ArrayElement, for sortings like RadixSort
+// write own generating function
 class SortBench {
     SortStats stats;
 
@@ -34,7 +34,7 @@ public:
 
     operator()(std::vector<size_t> arrays_sizes)
     {
-        for(auto size : arrays_sizes)
+        for (auto size : arrays_sizes)
         {
             measure(size);
         }
@@ -42,7 +42,7 @@ public:
 
     operator()(size_t array_size, size_t msr_num)
     {
-        while(msr_num--)
+        while (msr_num--)
         {
             measure(array_size);
         }
@@ -51,10 +51,10 @@ public:
 private:
     void measure(size_t size)
     {
-            auto tm          {test_single_time(size)};
-            auto [cmp, asgn] {test_single_cmp_asgn(size)};
+        auto tm          {test_single_time(size)};
+        auto [cmp, asgn] {test_single_cmp_asgn(size)};
 
-            stats.push_back({size, tm, {cmp, asgn}});
+        stats.push_back({size, tm, {cmp, asgn}});
     }
 
     std::pair<size_t, size_t> test_single_cmp_asgn(size_t len)
@@ -76,7 +76,69 @@ private:
 
         return end - start; // std::chrono:duration
     }
+    
+    template <typename T>
+    class GenFunc
+    {
+        static constexpr size_t Tsz       {sizeof(T)};
+        static constexpr size_t ui32sz    {sizeof(uint32_t)};
+        static constexpr size_t buff_len  {Tsz < ui32sz ? 1 : Tsz / ui32sz};
+        static constexpr size_t mersenne_twister_full_length_seed {624};
+
+        std::mt19937 generator;
+    public:
+
+        GenFunc()
+        {
+            std::random_device rd;
+
+            uint32_t numbers[mersenne_twister_full_length_seed];
+            std::generate(numbers, std::end(numbers), std::ref(rd));
+
+            SeedSeq sseq(numbers, std::end(numbers));
+            generator = std::mt19937{sseq};
+        }
+
+        T operator()()
+        {
+            auto buffer {gen_buffer().data()};
+
+            return *reinterpret_cast<T *>(buffer);
+        }
+    private:
+        auto gen_buffer()
+        {
+            std::array<uint32_t, buff_len> arr {};
+            std::generate(std::begin(arr), std::end(arr), std::ref(generator));
+
+            return arr;
+        }
+
+        template <class It>
+        class SeedSeq
+        {
+            It from;
+            It to;
+        public:
+
+            SeedSeq(It from, It to)
+                : from{from}, to{to} {}
+
+            template <typename It2>
+            void generate(It2 b, It2 e)
+            {
+                assert((e - b) <= (to - from));
+                std::copy(from, from + (e - b), b);
+            }
+        };
+    };
 };
+
+template <typename T>
+class GenFunctor
+{
+
+}
 
 };
 
