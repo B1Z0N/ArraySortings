c26c8f7918b2290e49c32010ba4d22dee3ce98b4
B1Z0N <kolausf@gmail.com>
2019-07-07 15:07:51 +0300

    Written tests and some helper functions for it

--- benchmark/tests/sort_bench_test.cpp
+++ benchmark/tests/sort_bench_test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <functional>
 #include <iterator>
+#include <vector>
 
 #include "../sort_bench.hpp"
 // #include "../../simple_sorting_methods/bubble_sort.hpp"
@@ -17,6 +18,75 @@ struct BubbleSort
 	}
 };
 
+
+
+template <
+    typename T
+    >
+std::ostream& __print_container(std::ostream& os, const T& val, const char* separator = ", ",
+                                const char *inscope = "[", const char* outscope = "]", bool end = false)
+// prints single value
+// end - tells if this the last element in the sequence
+{
+	os << val;
+	if (!end)
+	{
+		// if this is not the end
+		// print separator too
+		os << separator;
+	}
+
+	return os;
+}
+
+template <
+    typename T,
+    template <typename> typename C
+    >
+std::ostream& __print_container(std::ostream& os, const C<T>& con,
+                                const char* separator = ", ", const char *inscope = "[", const char* outscope = "]", bool end = false)
+// prints iterable of iterbles/values of ...
+// end - tells if this the last element in the sequence
+{
+	if (con.empty())
+	{
+		return os << inscope << " " << outscope;
+	}
+	auto prev_end {std::rbegin(con)};
+
+	os << inscope;
+
+	for (auto it {std::begin(con)}; it != std::prev(prev_end.base()); it++)
+	{
+		__print_container(os, *it, separator, inscope, outscope);
+	}
+
+	__print_container(os, *prev_end, separator, inscope, outscope, true);
+
+	os << outscope;
+
+	if (!end)
+	{
+		// if this is not the end
+		// print separator too
+		os << separator << std::endl;
+	}
+
+	return os;
+}
+
+template <
+    typename T,
+    template <typename> typename C
+    >
+std::ostream& print_container(std::ostream& os, const C<T>& con,
+                              const char* separator = ", ", const char *inscope = "[", const char* outscope = "]")
+// wrapper for prettyness
+{
+	return __print_container(os, con, separator, inscope, outscope, false);
+}
+
+
 std::ostream& ssoutput(std::ostream& os, const srtbch::SortStats& ss)
 {
 	using namespace std::chrono;
@@ -32,18 +102,8 @@ std::ostream& ssoutput(std::ostream& os, const srtbch::SortStats& ss)
 	return os;
 }
 
-std::ostream& standard_ssoutput(const srtbch::SortStats& ss) { return ssoutput(std::cout, ss); }
+auto& standard_ssoutput(const srtbch::SortStats& ss) { return ssoutput(std::cout, ss); }
 
-template <typename T, template <typename> typename C>
-std::ostream& print_container(std::ostream& os, C<T> con)
-{
-	os << "[";
-	std::ostream_iterator<T> oi {os, ", "};
-	std::copy(std::begin(con), std::end(con), oi);
-	os << "]";
-
-	return os;
-}
 
 template <
 	typename T, 
@@ -52,20 +112,20 @@ template <
 	>
 void test_sort(size_t array_size, size_t measure_num)
 {
-	srtbch::SortBench<T, Sort, GenF> srt {};
+	srtbch::SortBench<T, Sort, GenF> srt {true};
 
 	auto res {srt(array_size, measure_num)};
 	standard_ssoutput(res);
-	std::cout << "And sorted container is: \n";
-	print_container(std::cout, srt.get_array());
+	std::cout << "And sorted containers is: \n";
+	print_container(std::cout, srt.arrays());
 	std::cout << std::endl;
 }
 
 void test_sort_ints()
 {
-	test_sort<int, BubbleSort>(4, 3);
+	// test_sort<int, BubbleSort>(4, 3);
 	test_sort<int, BubbleSort>(25, 3);
-	test_sort<int, BubbleSort>(100, 3);
+	// test_sort<int, BubbleSort>(100, 3);
 }
 
 int main()
