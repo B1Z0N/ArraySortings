2312f67cc6217c036571a614aa030e63c718c511
B1Z0N <kolausf@gmail.com>
2019-09-14 22:50:50 +0300

    Added comparison tests

--- test/array_element_test.cpp
+++ test/array_element_test.cpp
@@ -10,13 +10,15 @@ using namespace srtbch;
 struct ResetFixture {
   ResetFixture() {
     ArrayElement<int>::reset();
-    ArrayElement<int> ae1;
   }
+  ArrayElement<int> ae1;
 };
+
 
 ////////////////////////////////////////////////////////////
 // Assignments tests
 ////////////////////////////////////////////////////////////
+
 
 TEST_CASE_METHOD(ResetFixture, "Basic Single ArrayElement Assignment",
                  "[basic assignment][single array element]") {
@@ -40,7 +42,7 @@ TEST_CASE_METHOD(ResetFixture, "Increment Single ArrayElement Assignment",
 }
 
 TEST_CASE_METHOD(ResetFixture,
-                 "Random Increment Single ArrayElement  Assignment",
+                 "Random Increment Single ArrayElement Assignment",
                  "[random increment assignment][single array element]") {
   // this test will be ran 5 times with random
   // maximal number of runs, from 1 to 1e5
@@ -69,16 +71,45 @@ TEST_CASE_METHOD(
   REQUIRE(ArrayElement<int>::get_asgn() == 2 * num);
 }
 
+
 ////////////////////////////////////////////////////////////
 // Comparisons tests
 ////////////////////////////////////////////////////////////
+
+
+static auto random_compare{[](const auto& fst, const auto& snd) {
+  static std::mt19937 mt{std::random_device{}()};
+  static std::uniform_int_distribution dis {1, 6};
+  unsigned int choice = dis(mt);
+
+  switch (choice) {
+    case 1: {
+      return fst > snd;
+    }
+    case 2: {
+      return fst < snd;
+    }
+    case 3: {
+      return fst >= snd;
+    }
+    case 4: {
+      return fst <= snd;
+    }
+    case 5: {
+      return fst == snd;
+    }
+    case 6: {
+      return fst != snd;
+    }
+  }
+}};
 
 TEST_CASE_METHOD(ResetFixture, "Basic Single ArrayElement Comparison",
                  "[basic comparison][single array element]") {
   REQUIRE(ArrayElement<int>::get_asgn() == 0);
 }
 
-TEST_CASE_METHOD(ResetFixture, "Increment Single ArrayElement Assignment",
+TEST_CASE_METHOD(ResetFixture, "Increment Single ArrayElement Comparison",
                  "[fixed increment comparison][single array element]") {
   ae1 > 3;
   REQUIRE(ArrayElement<int>::get_cmp() == 1);
@@ -87,38 +118,11 @@ TEST_CASE_METHOD(ResetFixture, "Increment Single ArrayElement Assignment",
 }
 
 TEST_CASE_METHOD(ResetFixture,
-                 "Random Increment Single ArrayElement  Assignment",
+                 "Random Increment Single ArrayElement Comparison",
                  "[random increment comparison][single array element]") {
   // this test will be ran 5 times with random
   // maximal number of runs, from 1 to 1e5
   std::size_t num = GENERATE(take(5, random(1, static_cast<int>(1e5))));
-
-  static auto random_compare{[](const auto& fst, const auto& snd) {
-    static std::mt19937 mt{std::random_device{}()};
-    static std::uniform_int_distribution{1, 6};
-    unsigned int choice = dis(mt);
-
-    switch (choice) {
-      case 1: {
-        return fst > snd;
-      }
-      case 2: {
-        return fst < snd;
-      }
-      case 3: {
-        return fst >= snd;
-      }
-      case 4: {
-        return fst <= snd;
-      }
-      case 5: {
-        return fst == snd;
-      }
-      case 6: {
-        return fst != snd;
-      }
-    }
-  }};
 
   for (std::size_t i = 0; i < num; ++i) {
     random_compare(ae1, 3);
@@ -128,20 +132,26 @@ TEST_CASE_METHOD(ResetFixture,
 }
 
 TEST_CASE_METHOD(
-    ResetFixture, "Random Increment Multiple ArrayElement`s",
-    "[random increment][multiple array elemens][move comparison]") {
+    ResetFixture, "Random Increment Multiple ArrayElement`s Comparison",
+    "[random increment][multiple array elemens]") {
   // this test will be ran 5 times with random
   // maximal number of runs, from 1 to 1e5
   std::size_t num = GENERATE(take(5, random(1, static_cast<int>(1e5))));
 
   for (std::size_t i = 0; i < num; ++i) {
     ArrayElement<int> ae2;
-    ae2 = std::move(ae1);
-    ae1 = 3;
+    random_compare(ae2, ae1);
+    random_compare(ae1, 3);
     REQUIRE(ArrayElement<int>::get_cmp() == 2 * (i + 1));
   }
   REQUIRE(ArrayElement<int>::get_cmp() == 2 * num);
 }
+
+
+////////////////////////////////////////////////////////////
+// Reset tests
+////////////////////////////////////////////////////////////
+
 
 TEST_CASE_METHOD(ResetFixture, "Assignments N Comparisons Reset", "[reset]") {
   ae1 = 3;
