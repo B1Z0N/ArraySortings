0977efbf51880c724a7b5d698c5a888c196d25ea
B1Z0N <kolausf@gmail.com>
2019-09-15 10:43:24 +0300

    Added is_sorted tests

--- include/sorting_benchmark/utility.hpp
+++ include/sorting_benchmark/utility.hpp
@@ -21,8 +21,8 @@ public:
     SeededMT19937() {
         std::random_device rd;
         std::array<int, std::mt19937::state_size> seed_data;
-        std::generate(seed_data.data(), seed_data.size(), std::ref(rd));
-        std::seed_seq seq {std::begin(seed_data), std::end(seed_data)};
+        std::generate(std::begin(seed_data), std::end(seed_data), std::ref(rd));
+        std::seed_seq seq (std::begin(seed_data), std::end(seed_data));
         std::mt19937 {seq};
     }
 };
--- test/utility_test.cpp
+++ test/utility_test.cpp
@@ -2,12 +2,27 @@
 
 #include "sorting_benchmark/utility.hpp"
 
-#include <functional>
 #include <array>
+#include <functional>
 
 using namespace srtbch;
 
-TEST_CASE("Is Sorted In Ascending Order(Default)", "[is sorted][ascending][default]") {
-  std::array arr {1, 3, 4, 6 ,10, 23};
+////////////////////////////////////////////////////////////
+// is_sorted tests
+////////////////////////////////////////////////////////////
+
+TEST_CASE("Base cases", "[basic]") {
+  std::array arr2{3};
+  REQUIRE(is_sorted(arr2.data(), arr2.size()) == true);
+  REQUIRE(is_sorted(arr2.data(), arr2.size(), std::greater<int>{}) == true);
+}
+
+TEST_CASE("Is Sorted In Ascending Order(Default)",
+          "[is sorted][ascending][default]") {
+  std::array arr{1, 3, 4, 6, 10, 23};
   REQUIRE(is_sorted(arr.data(), arr.size()) == true);
+  arr[0] = 5;
+  REQUIRE(is_sorted(arr.data(), arr.size()) == false);
+  arr[0] = 1;
+  REQUIRE(is_sorted(arr.data(), arr.size(), std::greater<int>{}) == false);
 }
